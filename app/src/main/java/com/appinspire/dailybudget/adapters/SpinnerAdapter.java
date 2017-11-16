d30660c4f20e8a19cf50f76fe23bcda0073ff164
Bilal rashid <bilal.rashid@hypernymbiz.com>
2017-11-17 00:11:26 +0500

    settings screen added and currencies added

--- app/src/main/java/com/appinspire/dailybudget/adapters/SpinnerAdapter.java
+++ app/src/main/java/com/appinspire/dailybudget/adapters/SpinnerAdapter.java
@@ -15,6 +15,9 @@ import com.appinspire.dailybudget.enumerations.SpinnerTypeEnum;
 import com.appinspire.dailybudget.models.Expense;
 import com.appinspire.dailybudget.utils.AppUtils;
 
+import java.util.ArrayList;
+import java.util.Currency;
+
 /**
  * Created by Bilal Rashid on 10/22/2017.
  */
@@ -23,11 +26,16 @@ public class SpinnerAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflter;
     int spinnerType;
+    ArrayList<Currency> currencies;
 
     public SpinnerAdapter(Context applicationContext, int spinnerType) {
         this.context = applicationContext;
         inflter = (LayoutInflater.from(applicationContext));
         this.spinnerType = spinnerType;
+        if(spinnerType == SpinnerTypeEnum.CURRENCY.getValue()){
+            currencies = new ArrayList<Currency>(Currency.getAvailableCurrencies());
+
+        }
     }
 
     @Override
@@ -36,6 +44,8 @@ public class SpinnerAdapter extends BaseAdapter {
             return IncomeEnum.values().length;
         else if(spinnerType == SpinnerTypeEnum.EXPENSE.getValue())
             return ExpenseEnum.values().length;
+        else if(spinnerType == SpinnerTypeEnum.CURRENCY.getValue())
+            return Currency.getAvailableCurrencies().size();
         return 0;
     }
 
@@ -51,16 +61,25 @@ public class SpinnerAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        view = inflter.inflate(R.layout.item_income_type_spinner, null);
-        ImageView icon = (ImageView) view.findViewById(R.id.image_type_icon);
-        TextView names = (TextView) view.findViewById(R.id.text_income_type);
-        if(spinnerType == SpinnerTypeEnum.EXPENSE.getValue()){
-            icon.setImageDrawable(AppUtils.getColorDrawable(ExpenseEnum.values()[i].getIconId(), context,false));
-            names.setText(ExpenseEnum.values()[i].getName());
-        }else if(spinnerType == SpinnerTypeEnum.INCOME.getValue()){
-            icon.setImageDrawable(AppUtils.getColorDrawable(IncomeEnum.values()[i].getIconId(), context,false));
-            names.setText(IncomeEnum.values()[i].getName());
+        if(spinnerType == SpinnerTypeEnum.CURRENCY.getValue()){
+            view = inflter.inflate(R.layout.item_currency, null);
+            TextView symbol = (TextView) view.findViewById(R.id.text_symbol);
+            TextView name = (TextView) view.findViewById(R.id.text_currency);
+            name.setText(currencies.get(i).getDisplayName());
+            symbol.setText(currencies.get(i).getSymbol());
+            return view;
+        }else {
+            view = inflter.inflate(R.layout.item_income_type_spinner, null);
+            ImageView icon = (ImageView) view.findViewById(R.id.image_type_icon);
+            TextView names = (TextView) view.findViewById(R.id.text_income_type);
+            if (spinnerType == SpinnerTypeEnum.EXPENSE.getValue()) {
+                icon.setImageDrawable(AppUtils.getColorDrawable(ExpenseEnum.values()[i].getIconId(), context, false));
+                names.setText(ExpenseEnum.values()[i].getName());
+            } else if (spinnerType == SpinnerTypeEnum.INCOME.getValue()) {
+                icon.setImageDrawable(AppUtils.getColorDrawable(IncomeEnum.values()[i].getIconId(), context, false));
+                names.setText(IncomeEnum.values()[i].getName());
+            }
+            return view;
         }
-        return view;
     }
 }
--- app/src/main/java/com/appinspire/dailybudget/fragments/SettingsFragment.java
+++ app/src/main/java/com/appinspire/dailybudget/fragments/SettingsFragment.java
@@ -3,19 +3,25 @@ package com.appinspire.dailybudget.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 import com.appinspire.dailybudget.R;
+import com.appinspire.dailybudget.adapters.SpinnerAdapter;
+import com.appinspire.dailybudget.enumerations.SpinnerTypeEnum;
 import com.appinspire.dailybudget.toolbox.ToolbarListener;
 
 /**
  * Created by Bilal Rashid on 10/16/2017.
  */
 
-public class SettingsFragment  extends Fragment implements View.OnClickListener{
+public class SettingsFragment  extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener{
     private ViewHolder mHolder;
+    private SpinnerAdapter mCurrencyAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,15 +43,34 @@ public class SettingsFragment  extends Fragment implements View.OnClickListener{
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mHolder = new ViewHolder(view);
+        mCurrencyAdapter = new SpinnerAdapter(getActivity(), SpinnerTypeEnum.CURRENCY.getValue());
+        mHolder.spinner.setAdapter(mCurrencyAdapter);
+        mHolder.spinner.setOnItemSelectedListener(this);
     }
     @Override
     public void onClick(View view) {
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
     public static class ViewHolder {
 
+        AppCompatSpinner spinner;
+        Button save,reset;
         public ViewHolder(View view) {
+            spinner = (AppCompatSpinner)view.findViewById(R.id.spinner_currency);
+            save = (Button)view.findViewById(R.id.button_save);
+            reset = (Button)view.findViewById(R.id.button_reset);
+
         }
 
     }
